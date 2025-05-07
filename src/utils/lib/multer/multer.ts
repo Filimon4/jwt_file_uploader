c38b77d6a19dd470529e9a0624394fc4897764c7
FILIMON <f.rykov@bk.ru>
2025-05-07 21:16:36 +0300

    auto folder create

--- src/utils/lib/multer/multer.ts
+++ src/utils/lib/multer/multer.ts
@@ -1,9 +1,28 @@
+import { access, mkdir } from "fs/promises";
 import multer from "multer";
+import path from "path";
 
 class MulterStorage {
+  static async getUploadDir(): Promise<string> {
+    const dir = process.env.UPLOAD_DIR;
+    if (!dir)
+      throw new Error("UPLOAD_DIR is not defined in environment variables");
+
+    const absolutePath = path.resolve(dir);
+    console.log(absolutePath)
+    try {
+      await access(absolutePath);
+    } catch {
+      console.log('mkdir')
+      await mkdir(absolutePath, { recursive: true });
+    }
+    return absolutePath;
+  }
+
   static storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, String(process.env.UPLOAD_DIR));
+    destination: async function (req, file, cb) {
+      const uploadPath = await MulterStorage.getUploadDir();
+      cb(null, uploadPath);
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
@@ -20,10 +39,16 @@ class MulterStorage {
     },
   });
 
-  static assertFile(data: unknown):asserts data is Express.Multer.File {
+  static assertFile(data: unknown): asserts data is Express.Multer.File {
     if (!data || typeof data !== "object") throw new Error(`Is not a File`);
-    const isFile = data as Express.Multer.File
-    if (!isFile['path'] || !isFile['mimetype'] || !isFile['size'] || !isFile['originalname']) throw new Error(`Is not a File`)
+    const isFile = data as Express.Multer.File;
+    if (
+      !isFile["path"] ||
+      !isFile["mimetype"] ||
+      !isFile["size"] ||
+      !isFile["originalname"]
+    )
+      throw new Error(`Is not a File`);
   }
 }
 
