a084695175c9cf8a8d98db5df2896848fe54fa77
FILIMON <f.rykov@bk.ru>
2025-05-08 15:04:23 +0300

    add class validator

--- src/models/user/user.entity.ts
+++ src/models/user/user.entity.ts
@@ -1,8 +1,10 @@
 import { Entity, PrimaryColumn, Column } from "typeorm";
+import { IsEmail } from "class-validator";
 
 @Entity()
 export class User {
   @PrimaryColumn()
+  @IsEmail()
   id: string;
 
   @Column()
